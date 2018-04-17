a912fd990727dd107532adbf54c9058caf11a0a8
ilyar <zz77zzgkilyar@163.com>
2018-04-17 18:29:48 +0800

    class Post extend Model
    获取数据 方法是Model的all方法
    post::all()

--- laravel_test/larapp/app/Post.php
+++ laravel_test/larapp/app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Table Name
+    // protected $table = 'posts';
+    //Primary Key
+    // protected $primaryKey = 'id';
+    //Timestamps
+    // public $Timestamps = true;
 }
+//Model下有哪些方法
+
